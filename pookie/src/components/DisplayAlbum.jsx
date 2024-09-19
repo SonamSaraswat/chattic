8b62d100be80b038be4cdf66612080e1b857eff4
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-09-19 20:49:53 +0530

    changes

--- pookie/src/components/DisplayAlbum.jsx
+++ pookie/src/components/DisplayAlbum.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import {assets, albumsData } from '../assets/assets'
+import {assets, albumsData,songsData } from '../assets/assets'
 import { useParams } from 'react-router-dom'
 
 
@@ -17,38 +17,22 @@ const DisplayAlbum = () => {
         <p>PlayList</p>
         <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
         <h4>{albumData.desc}</h4>
-        <p className='mt-1'>
-          <img className='inline-block w-5' src={assets.spotify_logo} alt=""/>
-          <b>Spotify</b>
-           1,323,154 likes
+        <p className='mt-1'>        
+          <b>1,323,154 likes</b>
            <b>50 songs</b>
-           about 2 hr 30 min
+             about 2 hr 30 min
         </p>
       </div>
     </div>
-    <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
-      <p><b className='mr-4'>#</b>Title</p>
-      <p>Album</p>
-      <p className='hidden sm:block'>Data Added</p>
-      <img className='m-auto w-4' src={assets.clock_icon} alt=""/>
-      </div>
-      <hr/>{
-        songsData.map((item,index)=>{
-         <div key={index} className='grid grid-cols-3 sm:gid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover-[#ffffff2b] cursor-pointer'>
-            <p className='text-white'>
-              <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
-              <img className='inline w-10 mr-5' src={item.image} alt="" />
-              {item.name}
-            </p>
-
-            <p className='text-[15px]'>{albumData.name}</p>
-            <p className='text-[15px] hidden sm:block'> 5 days ago</p>
-            <p className='text-[15px] text-center'>{item.duration}</p>
-
-          </div>
-
-        })
-      }
+    <br></br>
+    <hr></hr>
+    <div className=' m-2 mb-2 p-2 pb-2 flex justify-around'>
+      <button className=' px-4 py-1.5 rounded text-black bg-white mt-4'>Open Chat</button>
+      <button className=' px-4 py-1.5 rounded text-black bg-white mt-4'>Add Comments</button>
+    </div>
+    <hr></hr>
+    
+      
 
    
     </>
